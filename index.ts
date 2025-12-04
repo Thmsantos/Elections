18fd796d23936472381111462d5c3d66b9fb1cf5
Thmsantos <thmessiasdev@gmail.com>
2025-12-04 16:52:58 -0300

    :recycle: refactor: format files

--- index.ts
+++ index.ts
@@ -1,20 +1,9 @@
 import { Elysia } from "elysia";
 import swagger from "@elysiajs/swagger";
 
-import { createCandidateController } from "./src/controllers/Candidate";
-import { takeVoteController } from "./src/controllers/TakeVote";
+import { candidateRoutes } from "./src/routes/candidateRoutes";
+import { voteRoutes } from "./src/routes/voteRoutes";
 
-import { createCandidateSchema, takeVoteSchema } from "./src/types/types";
-
-new Elysia()
-  .use(swagger())
-
-  .post("/create", createCandidateController.create, { body: createCandidateSchema })
-  .get("/candidates", createCandidateController.list)
-  .get("/results", createCandidateController.results)
-
-  .post("/vote", takeVoteController.vote, { body: takeVoteSchema })
-
-  .listen(3200);
+new Elysia().use(swagger()).use(candidateRoutes).use(voteRoutes).listen(3200);
 
 console.log("Server running at http://127.0.0.1:3200");
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,21 +1,21 @@
-import { t } from 'elysia'
+import { t } from "elysia";
 
 export const candidateSchema = t.Object({
-    id: t.Integer(),
-    name: t.String(),
-    votes: t.Integer(),
-})
+  id: t.Integer(),
+  name: t.String(),
+  votes: t.Integer(),
+});
 
 export const createCandidateSchema = t.Object({
-    name: t.String(),
-})
+  name: t.String(),
+});
 
 export const takeVoteSchema = t.Object({
-    name: t.String(),
-})
+  name: t.String(),
+});
 
-export type Candidate = typeof candidateSchema.static
+export type Candidate = typeof candidateSchema.static;
 
-export type CandidateBody = typeof createCandidateSchema.static
+export type CandidateBody = typeof createCandidateSchema.static;
 
-export type TakeVoteBody = typeof takeVoteSchema.static
+export type TakeVoteBody = typeof takeVoteSchema.static;
